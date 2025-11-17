b4c7bb9461ff12c5b5b1fb4337818aa9253a704f
KhajitCanCode <194299650+KhajitCanCode@users.noreply.github.com>
2025-11-17 17:32:21 -0500

    Implement authentication with private routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,69 @@ import './App.css'
 import BlogPostsPage from './pages/BlogPostsPage'
 import IndividualPostPage from './pages/IndividualPostPage'
 import ContactPage from './pages/ContactPage'
+import LoginPage from './pages/LoginPage'
 import { ThemeProvider } from './context/ThemeContext'
+import { AuthProvider, useAuth } from './context/AuthContext'
+
+// this is a wrapper component for protected routes
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth()
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<BlogPostsPage />} />
-          <Route path="/posts" element={<BlogPostsPage />} />
-          <Route path="/posts/:id" element={<IndividualPostPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            {/* Public route */}
+            <Route path="/login" element={<LoginPage />} />
+
+            {/* Protected routes */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <BlogPostsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/posts"
+              element={
+                <PrivateRoute>
+                  <BlogPostsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/posts/:id"
+              element={
+                <PrivateRoute>
+                  <IndividualPostPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <PrivateRoute>
+                  <ContactPage />
+                </PrivateRoute>
+              }
+            />
+
+            {/* Catch-all */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   )
 }
